2adc1cf7a569ac2ce6a45308047724b7ed694933
nikdelvin <erter.live@gmail.com>
2024-03-08 22:55:15 +0400

    Main Page Fix (Add tailyUi Layout)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss'
-import { tailyUI } from 'taily-ui'
+import { tailyUI, safeList } from 'taily-ui'
 
 const config: Config = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {}
     },
-    plugins: [tailyUI]
+    plugins: [tailyUI],
+    safelist: safeList
 }
 export default config
