b65c2f363b7183925fd500332d71a021d192b2cb
nikdelvin <erter.live@gmail.com>
2024-03-15 20:57:38 +0400

    Config Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-    integrations: [tailwind(), icon()]
+    integrations: [tailwind()]
 })
