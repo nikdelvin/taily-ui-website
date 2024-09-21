230b19dc8358e22e8bbaeece56e704c08e2ee281
nikdelvin <erter.live@gmail.com>
2024-09-21 21:44:19 +0400

    Add new block + fix icons

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 
 export default {
-    plugins: [autoprefixer]
+    plugins: {
+        autoprefixer,
+        tailwindcss
+    }
 }
