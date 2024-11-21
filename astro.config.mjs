5ef18034f0c7020f53ef6c72e3f0926d0314a129
nikdelvin <erter.live@gmail.com>
2024-11-21 11:52:13 +0400

    Domain update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-    site: 'https://tailyui.nikdelvin.com',
+    site: 'https://tailyui.by.nikdelv.in',
     integrations: [tailwind()]
 })
